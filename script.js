595459cdcfee854d78af4b3acc81681fb4c93a4d
GMoneyHoops <112903459+GMoneyHoops@users.noreply.github.com>
2023-05-04 13:21:27 -0400

    fixing syntax error on the else function

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ function onClick() {
       time_seconds = 0;
       document.getElementById("TimerId").style.color = "#ff0000";
     } else {
-        var time_seconds = 0;
-        downloadTimerId = setInterval(function {
-          document.getElementById("TimerId").innerHTML = time_seconds;
-          time_seconds += 1;
-        }, 1000);
+      var time_seconds = 0;
+      downloadTimerId = setInterval(function (){
+        document.getElementById("TimerId").innerHTML = time_seconds;
+      time_seconds += 1;
+      }, 1000);
   }};
