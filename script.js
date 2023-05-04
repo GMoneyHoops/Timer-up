39334d848fef1ee224cc2ca267b5b498fb4b4731
GMoneyHoops <112903459+GMoneyHoops@users.noreply.github.com>
2023-05-04 13:18:41 -0400

    timer now pauses on second click!

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-var timesClicked = 0;
-
-$("#Container").click(function() {
-    clicks++;
-
-    if (clicks>1) {
-    Container.addEventListener('click', function() {
-    document.getElementById("TimerId").innerHTML = "Stopped";
-    })}
-})
+let Container = document.querySelector('#Container');
+function onClick() {
+    clicks += 1;
+    let TimerId // 
+    if (clicks= 1) {
+      clearInterval(downloadTimerId);
+      time_seconds = 0;
+      document.getElementById("TimerId").style.color = "#ff0000";
+    } else {
+        var time_seconds = 0;
+        downloadTimerId = setInterval(function {
+          document.getElementById("TimerId").innerHTML = time_seconds;
+          time_seconds += 1;
+        }, 1000);
+  }};
